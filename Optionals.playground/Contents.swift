00b55dda7a16f79718738fb62320cd4ab2df1b17
Dakota Kim <codingvampire@gmail.com>
2020-02-07 12:31:36 -0500

    clean up comments + examples

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -2,9 +2,9 @@
 // iOS MVD Course
 // Created by Dakota Kim on 2/6/2020
 
-// Optionals are a type, similar to nullable values from Kotlin or
+// Optionals are a type, similar to nullable values from Kotlin
 // If a variable is optional, it will either have a value or none at all.
-// If a variable is not optional, you can be sure it holds an integer value.
+// If a variable is not optional, you can be sure it holds a value of whatever underlying type the variable is.
 
 var age : Int = 4 // Every person has an age.
 
@@ -27,19 +27,38 @@ favoriteNumber = 0 // Even when we assign a value to this variable, the type is 
 // at that point. If it does not, your app will crash. You can force unwrap an optional
 // using the ! operator at the end of the variable name.
 
+// A lot of style guides + dev teams ban forced unwrapping because its so risky.
+
 var favoriteColor : String? = "Mint"
 
 print("My favorite color is \(favoriteColor!)") // Since we know for certain this optional contains a value, this is okay.
 
+print("My favorite color might be \(favoriteColor)")
+
 // ------------------------
 
-// Optional Binding - "I think the box has something in it, so let's openly it slowly. If it has a value, let's assign it to a non-optional variable. If not, we'll handle the nil."
+// Optional Binding - "The box might or might not have something in it, so let's openly it slowly.
+// If it has a value, let's assign it to a non-optional variable. If not, we'll handle the nil."
 //
-// This is a better approach than force unwrapping as it gives developers a say in how their app should handle the nil.
+// This is a safer approach than force unwrapping as it gives developers a say in how their app should handle the nil.
 // Optional binding is the recommended way of unwrapping optionals.
 // This method is safer than force unwrapping and implicit unwrapping.
 
-var favoriteBeatlesSong : String? = "Lucy in the Sky with Diamonds"
+var favoriteBeatlesSong: String? = "Lucy in the Sky with Diamonds"
+
+// One approach
+// Below we start with a check to see if the value is nil or not.
+// While this approach may work, there are some weaknesses.
+// When I want to use the value in the following code block, I have to force unwrap it. (!)
+// If you forget the if-else check at any point where you want to use the optional, your app will crash.
+
+if favoriteBeatlesSong != nil {
+    print("My favorite song by the Beatles is \(favoriteBeatlesSong!)")
+} else {
+    print("I'm not a fan of the Beatles.")
+}
+
+// Optional Binding solves both of these problems.
 
 if let beatlesSong = favoriteBeatlesSong {
     // The above statement checks if favoriteBeatlesSong contains a value, and if it does, it will assign it to the constant beatlesSong.
@@ -50,8 +69,7 @@ if let beatlesSong = favoriteBeatlesSong {
     print("I'm not a fan of the Beatles.")
 }
 
-// The above solution helps us unwrap the optional into a value that we can use within these codeblocks. But once the if-else block has been executed, the
-// constant is no longer in memory/able to be used elsewhere.
+// The above solution helps us unwrap the optional into a value that's only defined within the if-block.
 // We have another tool to safely unwrap these optionals and use the resulting data elsewhere in our applications.
 
 var middleName : String? = "Cory"
@@ -63,6 +81,27 @@ func shoutMiddleName(optionalMiddleName: String?) {
     // So we can use middleName outside of the 'if let' and 'else' code blocks from the previous example.
 }
 
+// Implicit Unwrapping
+// "Like regular optionals, implicitly unwrapped optionals might contain a value or they might be nil.
+// However, unlike regular optionals you don’t need to unwrap them in order to use them: you can use them
+// as if they weren’t optional at all." - Paul Hudson
+//
+// Developers can declare an implicitly unwrapped optional by using the ! operator in place of ? when declaring the Type.
+
+let regularOptionalUsername: String? = nil // Needs to be unwrapped
+let implicitUsername: String! = "LehkaP" // Unwrapped implicitly
+
+class GitHubUser {
+    var username: String
+    init(_ username: String) {
+        self.username = username
+    }
+}
+
+let preeti = GitHubUser(implicitUsername)
+print(preeti.username)
+
+
 // ------------------------
 
 // Nil coalescing
@@ -70,10 +109,10 @@ func shoutMiddleName(optionalMiddleName: String?) {
 
 var firstName : String? = "Dakota"
 
-let unwrappedFirtName = firstName ?? "Anonymous"
+let unwrappedFirstName = firstName ?? "Anonymous"
 
-print("My first name is \(unwrappedFirtName)")
-// If there is a last name, we will assign it to unwrappedLastName. If the optional is nil, we assign a default last name of Smith.
+print("My first name is \(unwrappedFirstName)")
+// If there is a first name, we will assign it to unwrappedFirstName. If the optional is nil, we assign a default first name of "Anonymous".
 // The '??' is known as the nil-coalescing operator. So when we reference unwrappedFirstName, we will DEFINITELY be working with a non-optional type.
 
 // ------------------------
